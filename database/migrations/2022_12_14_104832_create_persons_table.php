3c6a41e9c7b9b1c421858697cb4200dc62a8436d
HanifNaf <70953834+HanifNaf@users.noreply.github.com>
2022-12-14 18:07:11 +0700

    Tambah Migration create_person_table

--- database/migrations/2022_12_14_104832_create_persons_table.php
+++ database/migrations/2022_12_14_104832_create_persons_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('order_id');
             $table->string('person_name');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('city');
+            $table->string('email');
             $table->foreign('order_id')
               ->references('id')->on('orderitems')->onDelete('cascade');
         });
